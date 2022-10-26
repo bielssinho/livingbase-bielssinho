32189ea0b5156e8ceb99d1fad92f669192f3b52b
Gabriel santos <bielnmjr@gmail.com>
2022-10-26 13:14:54 -0300

    feat: html post e estilização post

--- src/script/index.js
+++ src/script/index.js
@@ -1,1 +1,40 @@
-/* Desenvolva seu script aqui */
+import { getNews } from "./request.js";
+
+const renderList = async () =>{
+    const listPost = await getNews()
+    console.log(listPost)
+    const ul = document.querySelector(".list-post");
+
+    ul.innerHTML = ""
+    // <li class="post">
+    // <img src="./src/img/Rectangle 1.png" alt="imagem post">
+    // <h2>Cuidados para manter com plantas dentro de apartamentos </h2>
+    // <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate quos rerum delectus porro repellendus maxime voluptate corrupti, possimus commodi tenetur pariatur iusto laboriosam labore consectetur facilis minima nisi vitae autem.</p>
+    // <span data-patch="id-do-post">Acessar conteúdo</span>
+    // </li>
+    listPost.forEach(element => {
+        const li = document.createElement("li");
+        li.classList.add("post");
+    
+        const img = document.createElement("img");
+        img.src = `${element.image}`;
+        img.alt = "imagem post"
+    
+        const h2 = document.createElement("h2");
+        h2.innerText = `${element.title}`;
+    
+        const p = document.createElement("p");
+        p.innerText = `${element.description}`;
+    
+        const span = document.createElement("span");
+        span.setAttribute("data-patch", `${element.id}`)
+        span.innerText = "Acessar conteúdo";
+        
+        li.append(img, h2, p, span);
+        ul.append(li)
+        console.log(ul)
+    });
+    
+}
+renderList()
+
---
+++ src/script/request.js
@@ -0,0 +1,38 @@
+const baseUrl = "https://m2-api-living.herokuapp.com/news";
+
+async function getNewsById(id){
+    try{
+        const request = await fetch(baseUrl + `/${id}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        const response = await request.json()
+
+        return response
+    }catch(err){
+        console.log(err);
+    }
+}
+
+async function getNews(){
+    try{
+        const request = await fetch(baseUrl, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        const response = await request.json()
+        
+        return response.news
+    }catch(err){
+        console.log(err)
+    }
+}
+
+export{
+    getNews,
+    getNewsById
+}
